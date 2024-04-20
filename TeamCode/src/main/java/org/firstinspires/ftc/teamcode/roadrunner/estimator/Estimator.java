b5c8a5fddab6beef5db17bcc01fc50dd9b994304
SquidCooki2 <zahishaikh@hotmail.com>
2024-04-20 00:23:56 -0400

    Removed odo + IMU localizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/estimator/Estimator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/estimator/Estimator.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.roadrunner.estimator;
 import com.acmerobotics.roadrunner.Pose2d;
 
 public class Estimator {
+    // If extended: returns localizerPose if estimate is not available and a new pose if it is.
+    // This class is generic and does not estimate.
     public Pose2d estimate(Pose2d localizerPose) {
         return localizerPose;
     }
